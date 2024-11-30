bb795217941a6a4cfab84f0d50563454c58dce88
Noemi <nasladieg@gmail.com>
2024-11-30 13:13:12 +0100

    In: #6 (REFACTOR) Move the file 'ListNurseScreen.kt' and 'Login.kt' to 'ui' package. - Simplified imports by using '*', reducing redundancy in the affected packages.

--- app/src/main/java/com/example/hospitalfrontend/ui/ListNurseScreen.kt
+++ app/src/main/java/com/example/hospitalfrontend/ui/ListNurseScreen.kt
@@ -1,4 +1,4 @@
-package com.example.hospitalfrontend
+package com.example.hospitalfrontend.ui
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
--- app/src/main/java/com/example/hospitalfrontend/ui/Login.kt
+++ app/src/main/java/com/example/hospitalfrontend/ui/Login.kt
@@ -1,38 +1,24 @@
-package com.example.hospitalfrontend
+package com.example.hospitalfrontend.ui
 
 import android.util.Log
 import android.widget.Toast
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.input.*
 import androidx.compose.ui.unit.dp
+import com.example.hospitalfrontend.R
 
 @Composable
 fun Image(
