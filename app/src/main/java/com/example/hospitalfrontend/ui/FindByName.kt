e853db6d8e83108a8f78268c891e06cada0d9ff6
Noemi <nasladieg@gmail.com>
2024-12-02 12:21:48 +0100

    In: #13 (REFACTOR) Organize main page layout, streamline imports, and restructure FindByName - Simplified library imports using wildcard `*`. - Moved the `FindByName` file to UI package.

--- app/src/main/java/com/example/hospitalfrontend/ui/FindByName.kt
+++ app/src/main/java/com/example/hospitalfrontend/ui/FindByName.kt
@@ -1,4 +1,4 @@
-package com.example.hospitalfrontend.ui.theme
+package com.example.hospitalfrontend.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -14,6 +14,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.hospitalfrontend.ui.theme.HospitalFrontEndTheme
 
 class FindByName : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
